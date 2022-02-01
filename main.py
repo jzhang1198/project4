06f83bfc50df9f33490ef855b1200ccceea3a840
jzhang1198 <jzhang1198@gmail.com>
2022-02-01 11:06:39 -0800

    Added comments to main.py

--- main.py
+++ main.py
@@ -13,22 +13,22 @@ def main():
     sequences.remove(path2data + 'Homo_sapiens_BRD2.fa')
     reference, _ = read_fasta(path2data + 'Homo_sapiens_BRD2.fa')
 
-    sub_matrix_file = '../project4/substitution_matrices/BLOSUM62.mat'
+    sub_matrix_file = '../project4/substitution_matrices/BLOSUM62.mat' #instantiate NeedlemanWunsch object
     gap_open = -10
     gap_extend = -1
     f = NeedlemanWunsch(sub_matrix_file, gap_open, gap_extend)
 
     scores_alignments = []
-    for sequence in sequences:
+    for sequence in sequences: #find alignment scores and alignments of human BRD2 with all other species
         splits = sequence.split('/')[-1].split('_')
         species = splits[0] + ' ' + splits[1]
         seq,_ = read_fasta(sequence)
         score,reference_align,seq_align = f.align(reference,seq)
         scores_alignments.append((score,reference_align,seq_align,species))
 
-    scores_alignments.sort(reverse=True)
+    scores_alignments.sort(reverse=True) #sort by descending alignment score
 
-    for i in range(0,len(scores_alignments)):
+    for i in range(0,len(scores_alignments)): #print alignment scores in descending order
         print(f'Alignment score between Homo sapiens BRD2 and {scores_alignments[i][-1]} BRD2 is: {scores_alignments[i][0]}')
         print('')
 
